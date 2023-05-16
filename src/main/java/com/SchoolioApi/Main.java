26340960f28a38b059bc4f8b4d95be4e59367a25
George Osborn <georgeosborn2002@gmail.com>
2023-05-16 21:47:38 +0100

    Add logging infrastructure

--- src/main/java/com/SchoolioApi/Main.java
+++ src/main/java/com/SchoolioApi/Main.java
@@ -5,10 +5,26 @@ import com.SchoolioApi.controllers.BearerByCreds;
 import com.SchoolioApi.controllers.BearerByRefresh;
 import com.SchoolioApi.controllers.Register;
 import com.SchoolioApi.controllers.LessonSchedule;
+import com.SchoolioApi.helpers.ConfigFile;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
 
 import static spark.Spark.*;
 
 public class Main {
+    private static final Logger LOGGER_ALL_LEVELS = LogManager.getRootLogger();
+    private static final Logger LOGGER_DEBUG = LogManager.getLogger("DebugLogger");
+    private static final Logger LOGGER = LogManager.getLogger("ConsoleLogger");
+
+    private static final String DEBUG_MODE = ConfigFile.config().getProperty("CONSOLE_DEBUG");
+    static {
+        if (!DEBUG_MODE.equals("on")) {
+            Configurator.setLevel("ConsoleLogger", Level.OFF);
+        }
+    }
+
     public static void main(String[] args) {
         before("/secured/*", new AuthenticationFilter());
 
@@ -19,5 +35,11 @@ public class Main {
         post("/get-bearer-by-creds", new BearerByCreds());
         post("/get-bearer-by-refresh", new BearerByRefresh());
         post("/register", new Register());
+    }
+
+    public static void logAll(Level level, Exception message) {
+        LOGGER_ALL_LEVELS.log(level, message);
+        LOGGER_DEBUG.log(level, message);
+        LOGGER.log(level, message);
     }
 }
